340795a678e7cfbc40b2b761ea6a38416cb122cd
AJ <anthonyfields1@gmail.com>
2020-01-31 07:29:34 -0500

    initial project

---
+++ KleoHelper/CommandLineInterpreter.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
+
+namespace KleoHelper
+{
+    class CommandLineInterpreter : INotifyPropertyChanged
+    {
+		#region EVENTS
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+        #endregion
+
+
+        #region PROPERTIES
+        private bool isGuiEnabled;
+		public bool IsGuiEnabled
+		{
+			get
+			{
+				return isGuiEnabled;
+			}
+			set
+			{
+				isGuiEnabled = value;
+				OnPropertyChanged("IsGuiEnabled");
+			}
+		}
+
+        public List<string> SourceFiles = new List<string>();
+		public string ZipOutput = null;
+        #endregion
+
+        public string DecryptFile(string file)
+		{
+			using Process p = new Process();
+			p.StartInfo.FileName = "cmd.exe";
+			p.StartInfo.Arguments = $@"/C gpg -d ""{file}"" > ""{file}.csv""";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.StartInfo.CreateNoWindow = true;
+			p.Start();
+			var output = p.StandardOutput.ReadToEnd();
+			output += p.StandardError.ReadToEnd();
+			p.WaitForExit();
+			return output;
+		}
+
+		public void SendToZip()
+		{
+			if (File.Exists(ZipOutput)) File.Delete(ZipOutput);
+
+			using FileStream zip = new FileStream(ZipOutput, FileMode.Create);
+			using ZipArchive archive = new ZipArchive(zip, ZipArchiveMode.Create);
+			foreach (var file in SourceFiles)
+			{
+				var entryName = $"{ParseFileNameToScreen(file)}.csv";
+				archive.CreateEntryFromFile($"{file}.csv", entryName);
+			}
+		}
+
+		public string ParseFileNameToScreen(string fullanme)
+		{
+			return fullanme.Substring(fullanme.LastIndexOf(@"\") + 1);
+		}
+
+	}
+}
---
+++ KleoHelper/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KleoHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly CommandLineInterpreter cli; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            cli = new CommandLineInterpreter { IsGuiEnabled = true };
+            buttonStack.DataContext = cli;
+        }
+
+        private void Source_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                Multiselect = true,
+                DefaultExt = ".pgp",
+                Filter = "PGP Encryption (.pgp)|*.pgp|Possibly Encrypted Files (.pgp,.gpg,.csv,.txt)|*.pgp;*.gpg;*.csv;*.txt|All Types|*.*"
+            };
+            cli.SourceFiles.Clear();
+            if (dlg.ShowDialog() == true)
+            {
+                foreach (var _ in dlg.FileNames)
+                {
+                    cli.SourceFiles.Add(_);
+                    TextOutput.Text += $"\n{_} Added!";
+                }
+            }
+        }
+
+        private void Output_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog
+            {
+                FileName = "Decrypted_Files",
+                DefaultExt = ".zip",
+                Filter = "Zip File (.zip)|*.zip",
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                cli.ZipOutput = dlg.FileName;
+            }
+            TextOutput.Text += $"\nSending output to: {cli.ZipOutput}";
+        }
+
+        private async void Run_Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextOutput.Text += "\nStarting process....";
+            if (cli.SourceFiles.Count > 0)
+            {
+                PrepareGUI(true);
+                foreach (var file in cli.SourceFiles)
+                {
+                    TextOutput.Text += $"\nWorking {file}";
+                    string lineOut = await Task.Run(() => cli.DecryptFile(file));
+                    TextOutput.Text += $"\n{lineOut}";
+                }
+                if (cli.ZipOutput != null)
+                {
+                    TextOutput.Text += "\nSending to zip....";
+                    await Task.Run(() => cli.SendToZip());
+                }
+                TextOutput.Text += "\nProcess Complete!";
+                PrepareGUI(false);
+            }
+            else TextOutput.Text += "\nPlease select files first";
+        }
+         private void PrepareGUI(bool isStarting)
+        {
+            if (isStarting)
+            {
+
+                ProcessRunning.Visibility = Visibility.Visible;
+                cli.IsGuiEnabled = false;
+            }
+            else
+            {
+
+                ProcessRunning.Visibility = Visibility.Hidden;
+                cli.IsGuiEnabled = true;
+            }
+        }
+    }
+}
